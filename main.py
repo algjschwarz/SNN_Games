c9f14063f4c1cc4844a3c5af3e642cc7956b2f37
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-06-16 14:33:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+creature_pos = [2, 2]
+creature_angle = 0.0
+food_pos = [15, 4]
+
+plt.plot(creature_pos[0], creature_pos[1], 'bo')
+plt.plot(food_pos[0], food_pos[1], 'go')
+plt.show()
