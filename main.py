0fd6ae2434a77e53a4bda5aa01210c1c1105b32b
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-06-17 09:05:10 -0500

    Added plot and food tracking test

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import time
 
-creature_pos = [2, 2]
-creature_angle = 0.0
-food_pos = [15, 4]
+creature_pos = np.random.randint(0, 20, size=2)
+food_pos = np.random.randint(0, 20, size=2)
+print(creature_pos, food_pos)
 
-plt.plot(creature_pos[0], creature_pos[1], 'bo')
-plt.plot(food_pos[0], food_pos[1], 'go')
-plt.show()
+fig, ax = plt.subplots(1, 1)
+plt.ion()
+
+def update_plot():
+    ax.cla()
+    ax.axis((0, 20, 0, 20))
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(2))
+    ax.plot(creature_pos[0], creature_pos[1], 'bs')
+    ax.plot(food_pos[0], food_pos[1], 'go')
+    plt.pause(1)
+
+while not np.array_equal(creature_pos, food_pos):
+    if creature_pos[0] < food_pos[0]:
+        creature_pos[0] += 1
+    elif creature_pos[0] > food_pos[0]:
+        creature_pos[0] -= 1
+    if creature_pos[1] < food_pos[1]:
+        creature_pos[1] += 1
+    elif creature_pos[1] > food_pos[1]:
+        creature_pos[1] -= 1
+    
+    update_plot()
