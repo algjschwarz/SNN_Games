56452885d60e130150c2a7268b0c263631574ae8
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-06-21 18:18:52 -0500

    Added energy costs to the creature

--- main.py
+++ main.py
@@ -43,14 +43,16 @@ class Brain():
                     synapse.target.add_voltage(synapse.weight)
                     queue.append(synapse.target)
 
-    def step(self, sensory_spike_array) -> np.array:
+    def step(self, sensory_spike_array):
         queued_neurons = []
         output_neuron_index = []
         output_spike_array = np.zeros(len(self.motor_neuron_index))
+        spike_count = 0
 
         for i in self.sensory_neuron_index:
             self.neurons[i].add_voltage(sensory_spike_array[i])
             if self.neurons[i].attempt_fire():
+                spike_count += 1
                 self.propagate(self.neurons[i], queued_neurons)
 
         while len(queued_neurons) != 0:
@@ -59,12 +61,13 @@ class Brain():
                 self.propagate(neuron, queued_neurons)
                 index = self.neurons.index(neuron)
                 if index in self.motor_neuron_index:
+                    spike_count += 1
                     output_neuron_index.append(index)
 
         for i in range(len(output_neuron_index)):
             output_spike_array[self.motor_neuron_index.index(output_neuron_index[i])] = 1
 
-        return output_spike_array
+        return output_spike_array, spike_count
 
 class Creature():
     def __init__(self):
@@ -85,6 +88,13 @@ class Creature():
         elif self.position[1] > closest_food.position[1]: sensory_spike_array[3] = 1
 
         return sensory_spike_array
+    
+    def eat_food(self, sensory_spike_array):
+        if max(sensory_spike_array) == 0:
+            energy = self.energy + 20
+            self.energy = max(0, min(energy, 30))
+            return True
+        return False
 
     def sense_home(self):
         pass
@@ -113,16 +123,22 @@ class Creature():
         return spike_array """
 
     def think(self, sensory_spike_array) -> np.array:
-        return self.brain.step(sensory_spike_array)
+        movement_commands, spike_count = self.brain.step(sensory_spike_array)
+        self.energy -= spike_count * .1
+        return movement_commands
 
     def move(self, movement_commands):
+        moved = False
         for i in range(len(movement_commands)):       
             if movement_commands[i] != 0:
+                moved = True
                 match i:
                     case 0: self.position[0] += 1
                     case 1: self.position[0] -= 1
                     case 2: self.position[1] += 1
                     case 3: self.position[1] -= 1
+        if moved:
+            self.energy -= 1
 
 class Food():
     def __init__(self):
@@ -158,11 +174,15 @@ def main():
     foods = [Food() for _ in range(3)]
     homes = [Home()]
 
-    while True:
+    while foods:
         objects = creatures + foods + homes
         sensory_data = creatures[0].smell_food(foods)
-        movement_data = creatures[0].think(sensory_data)
-        creatures[0].move(movement_data)
+        if creatures[0].eat_food(sensory_data):
+            foods = [f for f in foods if not np.array_equal(creatures[0].position, f.position)]
+        else:
+            movement_data = creatures[0].think(sensory_data)
+            creatures[0].move(movement_data)
+        print(creatures[0].energy)
         
         update_plot(objects)
 main()
